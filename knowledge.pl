7565248a45d0ab67cd000174c3fd5160f8232abd
pgivens4915 <pgivens4915@gmail.com>
2014-04-27 12:22:50 -0600

    Done with lesson 5

--- knowledge.pl
+++ knowledge.pl
@@ -1,9 +1,7 @@
 % Prolog
-woman(mia).
-woman(jody).
-woman(yolanda).
-
 loves(vincent, mia).
 loves(marsellus, mia).
 loves(pumpkin, honey_bunny).
 loves(honey_bunny, pumpkin).
+
+jealous(X,Y):- loves(X,Z), loves(Y,Z).
