587b05174866bda1051ad24f65a4a334abf01dba
pgivens4915 <pgivens4915@gmail.com>
2014-04-27 11:58:01 -0600

    done with lesson 3

--- knowledge.pl
+++ knowledge.pl
@@ -1,6 +1,9 @@
 % Prolog
-happy(yolanda).
-listens2Music(mia).
-listens2Music(yolanda):- happy(yolanda).
-playsAirGuitar(mia):- listens2Music(mia).
-playsAirGuitar(yolanda):- listens2Music(yolanda).
+happy(vincent).
+listens2Music(butch).
+playsAirGuitar(vincent):-
+  listens2Music(vincent),
+  happy(vincent).
+playsAirGuitar(butch):-
+  happy(butch);
+  listens2Music(butch).
