9a7d8cac596c8c92b7f7ec0f478b2e5e075c5239
pgivens4915 <pgivens4915@gmail.com>
2014-05-03 11:52:04 -0600

    Exercise 2.3

--- knowledge.pl
+++ knowledge.pl
@@ -1,8 +1,15 @@
 % Prolog
-houseElf(dobby).
-witch(hermoione).
-witch('McGonagall').
-witch(ritaSkeeter).
-magic(X) :- houseElf(X).
-magic(X) :- wizard(X).
-magic(X) :- witch(X).
+word(determiner, a).
+word(determiner, every).
+word(noun, 'big kahuna burger').
+word(noun, criminal).
+word(verb, eats).
+word(verb, likes).
+
+sentence(W1, W2, W3, W4, W5):-
+  word(determiner, W1),
+  word(noun, W2),
+  word(verb, W3),
+  word(determiner, W4),
+  word(noun, W5),
+  nl.
